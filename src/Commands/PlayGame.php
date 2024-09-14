e323ab2425c8953b9fd81ed314e897a78d9ea48c
Andrew Masiye <amasiye313@gmail.com>
2024-09-14 21:18:44 +0200

    refactor(util/config): improve PlayGame.php command

--- src/Commands/PlayGame.php
+++ src/Commands/PlayGame.php
@@ -2,6 +2,7 @@
 
 namespace Sendama\Console\Commands;
 
+use Dotenv\Dotenv;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -24,28 +25,41 @@ class PlayGame extends Command
   {
     $directory = $input->getOption('directory') ?? '.';
     $sendamaConfigFilename = 'sendama.json';
+    $sendamaDotEnvFilename = '.env';
 
-    if (! $this->isValidDirectory($directory) )
-    {
+    if (! $this->isValidDirectory($directory) ) {
       $output->writeln('Invalid Sendama game directory.');
       return Command::FAILURE;
     }
 
-    $output->writeln('Starting the game...');
-    $output->writeln('Game directory: ' . $directory);
+    // Load the .env file
+    if (file_exists($directory . '/' . $sendamaDotEnvFilename) ) {
+      $dotenv = Dotenv::createImmutable($directory);
+      $dotenv->load();
+    }
 
     // Open the project config and retrieve the main file
-    $config = json_decode(file_get_contents($directory . '/' . $sendamaConfigFilename), true);
+    $config = json_decode(file_get_contents($directory . '/' . $sendamaConfigFilename));
 
-    if (! file_exists($directory . '/' . $config['main']) )
-    {
+    if (! file_exists($directory . '/' . $config->main) ) {
       $output->writeln('Main file not found.');
       return Command::FAILURE;
     }
 
+    if ( env('DEBUG_MODE', false) ) {
+      $gameName = $config->name ?? env('GAME_NAME', 'Sendama Game');
+      $output->writeln([
+        "<fg=gray>[DEBUG_MODE] enabled</>",
+        "<comment>Starting Sendama project...</comment>",
+        "",
+        "Game name: <fg=gray>$gameName</>",
+        "Game directory: <fg=gray>$directory</>"
+      ]);
+      usleep(2500000);
+    }
+
     // Start the game using the main file
-    if (false === passthru("php $directory/{$config['main']}" ) )
-    {
+    if (false === passthru("php $directory/$config->main" ) ) {
       $output->writeln('Failed to start the game.');
       return Command::FAILURE;
     }
